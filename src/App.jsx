8269624a4e35dd47404658652ac8388efdd0e0dc
khairallah17 <med.khairallah3@gmail.com>
2023-04-30 12:02:25 +0100

    Add new Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Index from './pages/index'
 import Cryptocurrencies from './pages/Cryptocurrencies'
 import Contact from './pages/Contact'
-import ContactInfo from './pages/ContactInfo'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import About from './pages/About'
+import Sell from './pages/Contact/Sell'
+import Buy from './pages/Contact/Buy'
+import OtherInquiries from './pages/Contact/OtherInquieries'
+import AfterTrade from './pages/Contact/AfterTrade'
 
 function App() {
 
@@ -15,7 +18,10 @@ function App() {
         <Route path="/" element={<Index/>}/>
         <Route path="/market" element={<Cryptocurrencies/>}/>
         <Route path="/contact" element={<Contact/>}/>
-        <Route path="/contacts/*" element={<ContactInfo/>}/>
+        <Route path="contact/sell" element={<Sell/>}/>
+        <Route path="contact/buy" element={<Buy/>}/>
+        <Route path="contact/otherinquieries" element={<OtherInquiries/>}/>
+        <Route path="contact/afterTrade" element={<AfterTrade/>}/>
         <Route path='/privacyPolicy' element={<PrivacyPolicy/>} />
         <Route path='/about' element={<About/>}/>
       </Routes>
